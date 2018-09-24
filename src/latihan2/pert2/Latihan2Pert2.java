672f8e4d0cc03ee9cb468b59f466a515744a6844
mif99ms <43515183+mif99ms@users.noreply.github.com>
2018-09-25 06:32:17 +0700

    Initial commit

---
+++ src/latihan2/pert2/Latihan2Pert2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2.pert2;
+
+/**
+ *
+ * @author 
+ */
+public class Latihan2Pert2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int nPanjang;
+        int nLebar;
+        int nLuas;
+        
+        nPanjang = 8;
+        nLebar = 5;
+        
+        nLuas = nPanjang * nLebar;
+        
+        System.out.println("cm dan lebar" + 
+                "Luas Pesegi Panjang dengan Panjang" + nPanjang + "cm dan lebar"+ nLebar +"cm adalah" + nLuas+"cm");
+        
+    }
+    
+}
